e408b3b1a86be9d460b617bd3196b30e9f05ddda
iagoalvarezzz <alvarezsolacheiago@gmail.com>
2024-11-28 12:43:21 +0100

    Add change size button

--- js-file.js
+++ js-file.js
@@ -3,15 +3,9 @@ document.addEventListener('DOMContentLoaded', function () {
 
 const container = document.querySelector(".container");
 const changeSize = document.querySelector(".changeSize");
+let size = 16;
 
-for(let i = 0; i < 16 * 16; i++){
-  const div = document.createElement("div");
-  div.classList.toggle("cell");
-  div.id = "cell" + i;
-  div.style.border = "2px solid black";
-  div.style.padding = "23px";
-  container.appendChild(div);
-}
+createNewGrid();
 
 
 
@@ -24,11 +18,31 @@ container.addEventListener("mouseover", (event) =>{
 changeSize.addEventListener("click",newSize);
 
 function newSize (){
-  let size = prompt("Insert new size");
+  size = prompt("Insert new size");
   while (size > 100)
     if (size > 100) size = prompt("It can't be over 100");
-  
+  deleteGrid();
+  createNewGrid();
+}
 
+function deleteGrid(){
+  for (let i = 1; i < size ** 2; i++){
+    while (container.firstChild) 
+      container.removeChild(container.firstChild);   
+  }
+}
+
+function createNewGrid(){
+  console.log("iej");
+  for(let i = 0; i < size ** 2; i++){
+    console.log("je");
+    const div = document.createElement("div");
+    div.classList.toggle("cell");
+    div.id = "cell" + i;
+    div.style.border = "1px solid black";
+    div.style.padding = (800 / size / 2) - 1 + "px";
+    container.appendChild(div);
+  }
 }
 
 });
